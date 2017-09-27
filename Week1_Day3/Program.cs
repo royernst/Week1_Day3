eca0ee6a89562f8b2e0c7003fcd83b09ce8d491b
royernst <roy.e31@hotmail.com>
2017-09-27 13:38:50 -0400

    Practice with age if-else

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -10,7 +10,7 @@ namespace Week1_Day3
     {
         static void Main(string[] args)
         {
-            
+            /*
             Console.WriteLine("Please enter a whole number");
             int firstNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter another whole number");
@@ -26,6 +26,33 @@ namespace Week1_Day3
             {
                 Console.WriteLine("Both numbers are equal!");
             };
+            
+
+
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
+
+            if (userAge == 0 || userAge == 1)
+            {
+                Console.WriteLine("You're just a baby!");
+                
+            } else if (userAge > 1 && userAge < 4)
+            {
+                Console.WriteLine("Yay toddlers!");
+            } else if (userAge >= 4 && userAge < 13)
+            {
+                Console.WriteLine("Enjoy your childhood!");
+            } else if (userAge >= 13 && userAge < 65)
+            {
+                Console.WriteLine("Life is awkward.");
+            } else if (userAge >= 65 && userAge < 100)
+            {
+                Console.WriteLine("Enjoy your retirement!");
+            } else
+            {
+                Console.WriteLine("That's not your real age");
+            }
+            */
         }
     }
 }
