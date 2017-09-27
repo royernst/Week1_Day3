d072e0b2dcd05ca21d971c5cbf2da7a94c8dbdb2
royernst <roy.e31@hotmail.com>
2017-09-27 13:08:40 -0400

    Started with if-else

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -10,10 +10,22 @@ namespace Week1_Day3
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Please enter a whole number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another whole number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-            string test = Console.ReadLine();
-            int value = Convert.ToInt32(test);
-            Console.WriteLine(test);
+            if(firstNumber > secondNumber)
+            {
+                Console.WriteLine("The first number is greater!");
+            } else if(firstNumber < secondNumber)
+            {
+                Console.WriteLine("The second number is greater!");
+            } else
+            {
+                Console.WriteLine("Both numbers are equal!");
+            };
         }
     }
 }
